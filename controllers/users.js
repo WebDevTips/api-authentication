cc2e5cf85d49b50e90a5de0bd2dd3d3fc960cb20
WebDevTips <prashantchaudhari8503@gmail.com>
2018-03-26 18:23:39 +0530

    Added Middlewares

---
+++ controllers/users.js
@@ -0,0 +1,13 @@
+module.exports = {
+    signUp: async (req, res, next) => {
+        console.log('UsersController.signUp() Called!!!');
+    },
+
+    signIn: async (req, res, next) => {
+        console.log('UsersController.signIn() Called!!!');
+    },
+
+    home: async (req, res, next) => {
+        console.log('UsersController.home() Called!!!');
+    },
+};
