13ec23dd8a3f3e646bd4b0a0dcc33e2c1a7078e6
WebDevTips <prashantchaudhari8503@gmail.com>
2018-03-26 20:05:23 +0530

    updated

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 module.exports = {
     signUp: async (req, res, next) => {
+        // validate email & password
         console.log('UsersController.signUp() Called!!!');
     },
 
     signIn: async (req, res, next) => {
+        // generate a token
         console.log('UsersController.signIn() Called!!!');
     },
 
